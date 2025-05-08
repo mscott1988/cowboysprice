16b72727d4b41b8b1c4512b944715952125d2bc9
mscott1988 <michael@cowboysac.com>
2025-05-08 17:24:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const units = {
+  "Robert Madden": {
+    "C5A1V": {
+      "label": "C5A1V Variable Speed AC",
+      "options": {
+        "2 Ton": {
+          "price": 2379,
+          "coil": "EAM5X24M14A",
+          "coil_price": 445,
+          "furnace": "G80CTL0701412B",
+          "furnace_price": 1460,
+          "seer2": 17.0,
+          "eer2": 10.5,
+          "ahri": "215691194",
+          "total_price": 4284,
+          "tax_credit": true
+        },
+        "3 Ton": {
+          "price": 2983,
+          "coil": "EAM5X36M17A",
+          "coil_price": 475,
+          "furnace": "G80CTL0902120B",
+          "furnace_price": 1580,
+          "seer2": 18.0,
+          "eer2": 11.0,
+          "ahri": "215696475",
+          "total_price": 5038,
+          "tax_credit": true
+        }
+      }
+    }
+  },
+  "Century HVAC Supplies": {
+    "YC6": {
+      "label": "YC6 2-Stage AC",
+      "options": {
+        "2 Ton": {
+          "price": 1703.84,
+          "coil": "CTM24A5ABS1",
+          "coil_price": 528.83,
+          "furnace": "Y81E060A12SMPS1",
+          "furnace_price": 846.57,
+          "seer2": 17.25,
+          "eer2": 12.75,
+          "ahri": "216600741",
+          "total_price": 3079.24,
+          "tax_credit": true
+        },
+        "3 Ton": {
+          "price": 1803.9,
+          "coil": "CTM36A5ABS1",
+          "coil_price": 560.25,
+          "furnace": "Y81E080A16SMPS1",
+          "furnace_price": 878.0,
+          "seer2": 17.5,
+          "eer2": 13.0,
+          "ahri": "216600999",
+          "total_price": 3242.15,
+          "tax_credit": true
+        }
+      }
+    }
+  }
+};
